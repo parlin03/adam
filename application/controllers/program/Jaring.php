52d2df8d8674fe5ac97a7a07e7844bb524cd6a3b
unknown <parlin03@gmail.com>
2023-01-06 22:15:20 +0800

    new file:   application/controllers/program/Jaring.php 	new file:   application/models/Jaring_model.php 	new file:   application/views/program/index.php

---
+++ application/controllers/program/Jaring.php
@@ -0,0 +1,305 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Jaring extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function Index()
+    {
+        $data['title'] = 'Jaring Program Makassar B';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array(); //arraynya sebaris
+        $this->load->helper('url');
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('program/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Index_list()
+    {
+        $this->load->model('Jaring_model', 'jaring');
+
+        $dpt = $this->jaring->getDataDpt();
+        // $categories = array();
+        // $categories['name'] = '';
+        $rows = array();
+        $rows['name'] = 'Total DPT';
+        $rows['type'] = 'column';
+        foreach ($dpt as $d) {
+            // $categories['categories'][] = $d->namakec;
+            $rows['data'][] = $d->total;
+        }
+
+        $target = $this->jaring->getDataPip();
+        $rows1 = array();
+        $rows1['name'] = 'Beasiswa PIP';
+        $rows1['type'] = 'column';
+        foreach ($target as $t) {
+            $rows1['data'][] =  $t->total;
+        }
+        $tercapai = $this->jaring->getDataKip();
+        $rows2 = array();
+        $rows2['name'] = 'Beasiswa KIP';
+        $rows2['type'] = 'column';
+        foreach ($tercapai as $c) {
+            $rows2['data'][] =  $c->total;
+        }
+        $ragu = $this->jaring->getDataBpum();
+        $rows3 = array();
+        $rows3['name'] = 'BPUM';
+        $rows3['type'] = 'column';
+        foreach ($ragu as $r) {
+            $rows3['data'][] = $r->total;
+        }
+        $result = array();
+        // array_push($result, $categories);
+        array_push($result, $rows);
+        array_push($result, $rows1);
+        array_push($result, $rows2);
+        array_push($result, $rows3);
+
+        print json_encode($result, JSON_NUMERIC_CHECK);
+    }
+
+
+    public function Panakukkang()
+    {
+        $data['title'] = 'Kontribusi Pemilih Kec. Panakkukang';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array(); //arraynya sebaris
+
+        $this->load->helper('url');
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('kontribusi/panakukkang', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Panakukkang_list()
+    {
+        $namakec = 'panakukkang';
+        $this->load->model('Kontribusi_model', 'kontribusi');
+        $dpt = $this->kontribusi->getDataDptKec($namakec);
+        // $categories = array();
+        // $categories['name'] = '';
+        $rows = array();
+        $rows['name'] = 'Total DPT';
+        $rows['type'] = 'column';
+        foreach ($dpt as $d) {
+            // $categories['categories'][] = $d->namakec;
+            // $rows['kel'][] = $d->namakel;
+            $rows['data'][] = $d->total;
+        }
+
+        $target = $this->kontribusi->getDataTargetKec($namakec);
+        $rows1 = array();
+        $rows1['name'] = 'Target';
+        $rows1['type'] = 'column';
+        foreach ($target as $t) {
+            $rows1['data'][] =  $t->total;
+        }
+        $tercapai = $this->kontribusi->getDataTercapaiKec($namakec);
+        $rows2 = array();
+        $rows2['name'] = 'Tercapai';
+        $rows2['type'] = 'line';
+        foreach ($tercapai as $c) {
+            $rows2['data'][] =  $c->total;
+        }
+        $ragu = $this->kontribusi->getDataRaguKec($namakec);
+        $rows3 = array();
+        $rows3['name'] = 'Ragu-Ragu';
+        $rows3['type'] = 'line';
+        foreach ($ragu as $r) {
+            $rows3['data'][] = $r->total;
+        }
+        $result = array();
+        // array_push($result, $categories);
+        array_push($result, $rows);
+        array_push($result, $rows1);
+        array_push($result, $rows2);
+        array_push($result, $rows3);
+
+        print json_encode($result, JSON_NUMERIC_CHECK);
+    }
+
+    public function Manggala()
+    {
+        $data['title'] = 'Kontribusi Pemilih Kec. Manggala';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array(); //arraynya sebaris
+
+        $this->load->helper('url');
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('kontribusi/manggala', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Manggala_list()
+    {
+        $namakec = 'manggala';
+        $this->load->model('Kontribusi_model', 'kontribusi');
+        $dpt = $this->kontribusi->getDataDptKec($namakec);
+        $rows = array();
+        $rows['name'] = 'Total DPT';
+        $rows['type'] = 'column';
+        foreach ($dpt as $d) {
+            // $categories['categories'][] = $d->namakec;
+            // $rows['kel'][] = $d->namakel;
+            $rows['data'][] = $d->total;
+        }
+
+        $target = $this->kontribusi->getDataTargetKec($namakec);
+        $rows1 = array();
+        $rows1['name'] = 'Target';
+        $rows1['type'] = 'column';
+        foreach ($target as $t) {
+            $rows1['data'][] =  $t->total;
+        }
+        $tercapai = $this->kontribusi->getDataTercapaiKec($namakec);
+        $rows2 = array();
+        $rows2['name'] = 'Tercapai';
+        $rows2['type'] = 'line';
+        foreach ($tercapai as $c) {
+            $rows2['data'][] =  $c->total;
+        }
+        $ragu = $this->kontribusi->getDataRaguKec($namakec);
+        $rows3 = array();
+        $rows3['name'] = 'Ragu-Ragu';
+        $rows3['type'] = 'line';
+        foreach ($ragu as $r) {
+            $rows3['data'][] = $r->total;
+        }
+        $result = array();
+        // array_push($result, $categories);
+        array_push($result, $rows);
+        array_push($result, $rows1);
+        array_push($result, $rows2);
+        array_push($result, $rows3);
+
+        print json_encode($result, JSON_NUMERIC_CHECK);
+    }
+
+    public function Biringkanaya()
+    {
+        $data['title'] = 'Kontribusi Pemilih Kec. Biringkanaya';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array(); //arraynya sebaris
+
+        $this->load->helper('url');
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('kontribusi/biringkanaya', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Biringkanaya_list()
+    {
+        $namakec = 'biringkanaya';
+        $this->load->model('Kontribusi_model', 'kontribusi');
+        $dpt = $this->kontribusi->getDataDptKec($namakec);
+        $rows = array();
+        $rows['name'] = 'Total DPT';
+        $rows['type'] = 'column';
+        foreach ($dpt as $d) {
+            // $categories['categories'][] = $d->namakec;
+            // $rows['kel'][] = $d->namakel;
+            $rows['data'][] = $d->total;
+        }
+
+        $target = $this->kontribusi->getDataTargetKec($namakec);
+        $rows1 = array();
+        $rows1['name'] = 'Target';
+        $rows1['type'] = 'column';
+        foreach ($target as $t) {
+            $rows1['data'][] =  $t->total;
+        }
+        $tercapai = $this->kontribusi->getDataTercapaiKec($namakec);
+        $rows2 = array();
+        $rows2['name'] = 'Tercapai';
+        $rows2['type'] = 'line';
+        foreach ($tercapai as $c) {
+            $rows2['data'][] =  $c->total;
+        }
+        $ragu = $this->kontribusi->getDataRaguKec($namakec);
+        $rows3 = array();
+        $rows3['name'] = 'Ragu-Ragu';
+        $rows3['type'] = 'line';
+        foreach ($ragu as $r) {
+            $rows3['data'][] = $r->total;
+        }
+        $result = array();
+        // array_push($result, $categories);
+        array_push($result, $rows);
+        array_push($result, $rows1);
+        array_push($result, $rows2);
+        array_push($result, $rows3);
+
+        print json_encode($result, JSON_NUMERIC_CHECK);
+    }
+
+    public function Tamalanrea()
+    {
+        $data['title'] = 'Kontribusi Pemilih Kec. Tamalanrea';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array(); //arraynya sebaris
+
+        $this->load->helper('url');
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('kontribusi/tamalanrea', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function Tamalanrea_list()
+    {
+        $namakec = 'tamalanrea';
+        $this->load->model('Kontribusi_model', 'kontribusi');
+        $dpt = $this->kontribusi->getDataDptKec($namakec);
+        $rows = array();
+        $rows['name'] = 'Total DPT';
+        $rows['type'] = 'column';
+        foreach ($dpt as $d) {
+            // $categories['categories'][] = $d->namakec;
+            // $rows['kel'][] = $d->namakel;
+            $rows['data'][] = $d->total;
+        }
+
+        $target = $this->kontribusi->getDataTargetKec($namakec);
+        $rows1 = array();
+        $rows1['name'] = 'Target';
+        $rows1['type'] = 'column';
+        foreach ($target as $t) {
+            $rows1['data'][] =  $t->total;
+        }
+        $tercapai = $this->kontribusi->getDataTercapaiKec($namakec);
+        $rows2 = array();
+        $rows2['name'] = 'Tercapai';
+        $rows2['type'] = 'line';
+        foreach ($tercapai as $c) {
+            $rows2['data'][] =  $c->total;
+        }
+        $ragu = $this->kontribusi->getDataRaguKec($namakec);
+        $rows3 = array();
+        $rows3['name'] = 'Ragu-Ragu';
+        $rows3['type'] = 'line';
+        foreach ($ragu as $r) {
+            $rows3['data'][] = $r->total;
+        }
+        $result = array();
+        // array_push($result, $categories);
+        array_push($result, $rows);
+        array_push($result, $rows1);
+        array_push($result, $rows2);
+        array_push($result, $rows3);
+
+        print json_encode($result, JSON_NUMERIC_CHECK);
+    }
+}
---
+++ application/models/Jaring_model.php
@@ -0,0 +1,96 @@
+<?php
+
+use LDAP\Result;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+class Jaring_model extends CI_Model
+{
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function getDataDpt()
+    {
+        $this->db->select('namakec, count(*) as total');
+        $this->db->from('dpt');
+        $this->db->group_by('namakec');
+        $this->db->order_by('namakec', 'DESC');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getDataKip()
+    {
+        $this->db->select('namakec, count(*) as total');
+        $this->db->from('tbl_kip');
+        $this->db->group_by('namakec');
+        $this->db->order_by('namakec', 'DESC');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getDataPip()
+    {
+        $this->db->select('kec_siswa, count(*) as total');
+        $this->db->from('tbl_pip');
+        $this->db->group_by('kec_siswa');
+        $this->db->order_by('kec_siswa', 'DESC');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getDataBpum()
+    {
+        $this->db->select('kecamatan, count(*) as total');
+        $this->db->from('tbl_bpum');
+        $this->db->group_by('kecamatan');
+        $this->db->order_by('kecamatan', 'DESC');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getDataDptKec($namakec)
+    {
+        $this->db->select('namakel, count(*) as total');
+        $this->db->from('dpt');
+        $this->db->where('namakec', $namakec);
+        $this->db->group_by('namakel');
+        $this->db->order_by('iddesa');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getDataTargetKec($namakec)
+    {
+        $this->db->select('namakel, round(count(*)/2+1,0) as total');
+        $this->db->from('dpt');
+        $this->db->where('namakec', $namakec);
+        $this->db->group_by('namakel');
+        $this->db->order_by('iddesa');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getDataTercapaiKec($namakec)
+    {
+        $this->db->select('namakel, round(count(*)/3) as total');
+        $this->db->from('dpt');
+        $this->db->where('namakec', $namakec);
+        $this->db->group_by('namakel');
+        $this->db->order_by('iddesa');
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function getDataRaguKec($namakec)
+    {
+        $this->db->select('namakel, round(count(*)/5) as total');
+        $this->db->from('dpt');
+        $this->db->where('namakec', $namakec);
+        $this->db->group_by('namakel');
+        $this->db->order_by('iddesa');
+        $query = $this->db->get();
+        return $query->result();
+    }
+}
