fed4f05f623c87c90447cc360a9d54327679947d
unknown <parlin03@gmail.com>
2023-01-19 20:26:57 +0800

    modified:   application/views/templates/header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -27,6 +27,7 @@
     <script src="<?= base_url('assets/') ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
+    <script src="<?= base_url('assets/') ?>js/sb-admin-2.js"></script>
     <script src="<?= base_url('assets/') ?>js/sb-admin-2.min.js"></script>
 
     <script src="<?php echo base_url('assets/jquery/jquery-2.2.3.min.js') ?>"></script>
