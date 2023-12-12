6d0875a736c3618750a19d90055b1627800636b2
unknown <parlin03@gmail.com>
2023-12-12 21:24:26 +0800

    home

--- application/models/Tim_Model.php
+++ application/models/Tim_Model.php
@@ -5,6 +5,8 @@ class Tim_model extends CI_Model
 {
     public function __construct()
     {
+        parent::__construct();
+
         $this->load->database();
     }
     public function getAdam21()
