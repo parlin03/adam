33b97da5fbd5fe13d40a0612162e23ebce4f0d34
unknown <parlin03@gmail.com>
2023-12-18 22:53:37 +0800

    new

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -13,81 +13,38 @@
          </li> -->
      </ul>
 
+     <ul class="navbar-nav">
+
+         <!-- <li class="nav-item d-none d-sm-inline-block">
+             <a href="#" class="nav-link">Contact</a>
+         </li> -->
+     </ul>
      <!-- SEARCH FORM -->
-     <!-- <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
+     <!-- <form class="form-inline ml-3"> -->
+     <!-- <div class="input-group input-group-sm">
         <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
         <div class="input-group-append">
           <button class="btn btn-navbar" type="submit">
             <i class="fas fa-search"></i>
           </button>
         </div>
-      </div>
-    </form> -->
+      </div> -->
+     <!-- </form> -->
 
      <!-- Right navbar links -->
      <ul class="navbar-nav ml-auto">
-         <!-- Messages Dropdown Menu -->
-         <li class="nav-item dropdown">
-             <a class="nav-link" data-toggle="dropdown" href="#">
-                 <i class="far fa-comments"></i>
-                 <span class="badge badge-danger navbar-badge">3</span>
-             </a>
-             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                 <a href="#" class="dropdown-item">
-                     <!-- Message Start -->
-                     <div class="media">
-                         <img src="<?= base_url('assets/') ?>dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                         <div class="media-body">
-                             <h3 class="dropdown-item-title">
-                                 Brad Diesel
-                                 <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                             </h3>
-                             <p class="text-sm">Call me whenever you can...</p>
-                             <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                         </div>
-                     </div>
-                     <!-- Message End -->
-                 </a>
-                 <div class="dropdown-divider"></div>
-                 <a href="#" class="dropdown-item">
-                     <!-- Message Start -->
-                     <div class="media">
-                         <img src="<?= base_url('assets/') ?>dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                         <div class="media-body">
-                             <h3 class="dropdown-item-title">
-                                 John Pierce
-                                 <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                             </h3>
-                             <p class="text-sm">I got your message bro</p>
-                             <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                         </div>
-                     </div>
-                     <!-- Message End -->
-                 </a>
-                 <div class="dropdown-divider"></div>
-                 <a href="#" class="dropdown-item">
-                     <!-- Message Start -->
-                     <div class="media">
-                         <img src="<?= base_url('assets/') ?>dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                         <div class="media-body">
-                             <h3 class="dropdown-item-title">
-                                 Nora Silvester
-                                 <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                             </h3>
-                             <p class="text-sm">The subject goes here</p>
-                             <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                         </div>
-                     </div>
-                     <!-- Message End -->
-                 </a>
-                 <div class="dropdown-divider"></div>
-                 <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-             </div>
+         <?php
+            $date = strtotime("February 14, 2024 8:00 AM");
+            $remaining = $date - time();
+            $days_remaining = floor($remaining / 86400);
+            $hours_remaining = floor(($remaining % 86400) / 3600);
+            ?>
+         <li class="nav-item text-danger">
+             <h3> <b><?= $days_remaining; ?></b> hari lagi..!!!</h3>
          </li>
          <!-- Notifications Dropdown Menu -->
          <li class="nav-item dropdown">
-             <a class="nav-link" data-toggle="dropdown" href="#">
+             <!-- <a class="nav-link" data-toggle="dropdown" href="#">
                  <i class="far fa-bell"></i>
                  <span class="badge badge-warning navbar-badge">15</span>
              </a>
@@ -110,7 +67,7 @@
                  </a>
                  <div class="dropdown-divider"></div>
                  <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-             </div>
+             </div> -->
          </li>
          <!-- Nav Item - User Information -->
          <li class="nav-item dropdown">
