8aa4db6b085aa66965e3c21fced97bbd1e7a9df8
unknown <parlin03@gmail.com>
2024-01-20 14:30:15 +0800

    dpt

--- application/views/master/dpt/index.php
+++ application/views/master/dpt/index.php
@@ -96,7 +96,8 @@
 <script>
     $(function() {
         $("#tabelDPT").DataTable({
-            "iDisplayStart": 20,
+            "iDisplayLength": 5,
+            "iDisplayStart": 0,
             "responsive": true,
             "lengthChange": false,
             "autoWidth": false,
@@ -108,74 +109,10 @@
                 "url": '<?php echo base_url(); ?>master/dpt/ajax_list',
                 "type": "POST"
             },
-            "buttons": [{
-                "extend": 'copy',
-                "action": newexportaction
-            }, {
-                "extend": 'csv',
-                "action": newexportaction
-            }, {
-                "extend": 'excel',
-                "text": 'Excel',
-                "titleAttr": 'Excel',
-                "action": newexportaction
-            }, {
-                "extend": 'pdf',
-                "action": newexportaction,
-                "orientation": 'landscape'
-            }, {
-                "extend": 'print',
-                "text": 'Print',
-                "titleAttr": 'Print',
-                "action": newexportaction
-            }],
+
             "dom": 'Bfrtip',
             "select": true,
             "serverSide": true
-
-        }).buttons().container().appendTo('#tabelDPT_wrapper .col-md-6:eq(0)');
-
-        function newexportaction(e, dt, button, config) {
-            var self = this;
-            var oldStart = dt.settings()[0]._iDisplayStart;
-            dt.one('preXhr', function(e, s, data) {
-                // Just this once, load all data from the server...
-                data.start = 0;
-                data.length = 2147483647;
-                dt.one('preDraw', function(e, settings) {
-                    // Call the original action function
-                    if (button[0].className.indexOf('buttons-copy') >= 0) {
-                        $.fn.dataTable.ext.buttons.copyHtml5.action.call(self, e, dt, button, config);
-                    } else if (button[0].className.indexOf('buttons-excel') >= 0) {
-                        $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ?
-                            $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config) :
-                            $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
-                    } else if (button[0].className.indexOf('buttons-csv') >= 0) {
-                        $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ?
-                            $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button, config) :
-                            $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button, config);
-                    } else if (button[0].className.indexOf('buttons-pdf') >= 0) {
-                        $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config) ?
-                            $.fn.dataTable.ext.buttons.pdfHtml5.action.call(self, e, dt, button, config) :
-                            $.fn.dataTable.ext.buttons.pdfFlash.action.call(self, e, dt, button, config);
-                    } else if (button[0].className.indexOf('buttons-print') >= 0) {
-                        $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
-                    }
-                    dt.one('preXhr', function(e, s, data) {
-                        // DataTables thinks the first item displayed is index 0, but we're not drawing that.
-                        // Set the property to what it was before exporting.
-                        settings._iDisplayStart = oldStart;
-                        data.start = oldStart;
-                    });
-                    // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
-                    setTimeout(dt.ajax.reload, 0);
-                    // Prevent rendering of the full data to the DOM
-                    return false;
-                });
-            });
-            // Requery the server with the new one-time export settings
-            dt.ajax.reload();
-        }
-
+        })
     });
 </script>
