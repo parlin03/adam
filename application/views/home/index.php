8551611abfd2edf4785689724dd2f5367dc0978a
unknown <parlin03@gmail.com>
2024-02-03 22:53:37 +0800

    home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -39,13 +39,16 @@
                     <!-- /.info-box -->
                 </div>
                 <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-2">
+                <div class="col-12 col-sm-6 col-md-3">
                     <div class="info-box mb-3">
                         <span class="info-box-icon bg-success elevation-1"><i class="fas fa-user-friends"></i></span>
 
                         <div class="info-box-content">
-                            <span class="info-box-text">Tim</span>
-                            <span class="info-box-number"> <?= number_format($this->db->count_all('organik') + $this->db->count_all('soa') + $this->db->count_all('simpul')); ?></span>
+                            <span class="info-box-text">Tim 50</span>
+                            <span class="info-box-number"><?= number_format($this->db->get_Where('lks_tim50', array())->num_rows()); ?></span>
+
+                            <!-- <span class="info-box-text">Tim</span>
+                            <span class="info-box-number"> <?= number_format($this->db->count_all('organik') + $this->db->count_all('soa') + $this->db->count_all('simpul')); ?></span> -->
                         </div>
                         <!-- /.info-box-content -->
                     </div>
@@ -53,7 +56,7 @@
                 </div>
                 <!-- /.col -->
                 <!-- /.col -->
-                <div class="col-12 col-sm-6 col-md-2">
+                <div class="col-12 col-sm-6 col-md-3">
                     <div class="info-box mb-3">
                         <span class="info-box-icon bg-info elevation-1"><i class="fas fa-tshirt"></i></span>
 
@@ -67,21 +70,7 @@
                 </div>
                 <!-- /.col -->
                 <!-- fix for small devices only -->
-                <div class="clearfix hidden-md-up"></div>
 
-                <div class="col-12 col-sm-6 col-md-2">
-                    <div class="info-box mb-3">
-                        <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-vote-yea"></i></span>
-
-                        <div class="info-box-content">
-                            <span class="info-box-text">Tim 50</span>
-                            <span class="info-box-number"><?= number_format($this->db->get_Where('lks_tim50', array())->num_rows()); ?></span>
-                        </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
-                </div>
-                <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
                     <div class="info-box mb-3">
                         <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-chart-line"></i></span>
